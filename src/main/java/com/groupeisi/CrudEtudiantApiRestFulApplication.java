380f878f91c0d4ceb825de62dbde3695a970b57e
Medy2468 <mnsangouisidk@groupeisi.com>
2024-02-11 00:37:00 +0000

    Api RestFul

---
+++ src/main/java/com/groupeisi/CrudEtudiantApiRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEtudiantApiRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEtudiantApiRestFulApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/groupeisi/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.groupeisi.repository;
+
+import com.groupeisi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
